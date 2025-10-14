5f71a67f98d77ac3d169b20a51e4ac865d3b5348
Arnyt0 <arnostgvpt@gmail.com>
2025-10-14 21:36:57 +0200

    Delete ulohy 1/uloha 3.py

--- ulohy 1/uloha 3.py
+++
@@ -1,9 +0,0 @@
-x= float(input("zadaj minimalnu hodnotu intervalu"))
-y= float(input("zadaj maximalnu hodnotu intervalu"))
-c= float(input("zadaj cislo"))
-if c<=x:
-    print("cislo nepatri do intervalu moc male")
-elif c>=y:
-    print("cislo nepatri do intervalu moc velke")
-else:
-    print("cislo patri do intervalu")
